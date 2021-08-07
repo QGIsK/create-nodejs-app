6760e4afb4a0d18dddc1ddc6325e1a8a91ed4c19
Demian <devaccdemiann@gmail.com>
2021-08-07 21:58:05 +0200

    Base of of package.json

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,14 +1,14 @@
-const { version } = require('../../package.json');
+const { version, name, repository, license } = require('../../package.json');
 const config = require('../config/config');
 
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-boilerplate API documentation',
+    title: `${name} API documentation`,
     version,
     license: {
-      name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE',
+      name: `${license}`,
+      url: `${repository}/blob/master/LICENSE`,
     },
   },
   servers: [
